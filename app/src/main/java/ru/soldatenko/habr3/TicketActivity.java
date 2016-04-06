ab472ecf49af990f4175721fdde65ece8c5929c2
Sam Soldatenko <sam@soldatenko.ru>
2016-04-06 13:27:25 +0600

    Setup basic application structure

---
+++ app/src/main/java/ru/soldatenko/habr3/TicketActivity.java
@@ -0,0 +1,13 @@
+package ru.soldatenko.habr3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket);
+    }
+}
---
+++ app/src/test/java/ru/soldatenko/habr3/ExampleUnitTest.java
@@ -0,0 +1,12 @@
+package ru.soldatenko.habr3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
