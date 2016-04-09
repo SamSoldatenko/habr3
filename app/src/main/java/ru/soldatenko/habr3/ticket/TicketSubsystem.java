740088cc3742f9747c76a7287a268bc0821fb29d
Sam Soldatenko <sam.ngs.ru@gmail.com>
2016-04-09 07:29:42 +0600

    Set TicketSubsystem load ticket from file on first start

--- app/src/main/java/ru/soldatenko/habr3/ticket/TicketSubsystem.java
+++ app/src/main/java/ru/soldatenko/habr3/ticket/TicketSubsystem.java
@@ -2,7 +2,11 @@ package ru.soldatenko.habr3.ticket;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.os.Handler;
 
+import com.google.inject.Inject;
+
+import javax.inject.Provider;
 import javax.inject.Singleton;
 
 import ru.soldatenko.habr3.BR;
@@ -10,6 +14,16 @@ import ru.soldatenko.habr3.BR;
 @Singleton
 public class TicketSubsystem extends BaseObservable {
     private Ticket ticket;
+
+    @Inject
+    public TicketSubsystem(Handler handler, final Provider<ReadTicketFromFile> task) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                task.get().execute();
+            }
+        });
+    }
 
     @Bindable
     public Ticket getTicket() {
--- app/src/test/java/ru/soldatenko/habr3/ticket/TicketSubsystemTest.java
+++ app/src/test/java/ru/soldatenko/habr3/ticket/TicketSubsystemTest.java
@@ -1,24 +1,55 @@
 package ru.soldatenko.habr3.ticket;
 
 import android.databinding.Observable.OnPropertyChangedCallback;
+import android.os.Handler;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.inject.Provider;
 
 import ru.soldatenko.habr3.BR;
 
 import static org.junit.Assert.assertSame;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TicketSubsystemTest {
+    @InjectMocks
     TicketSubsystem subsystem;
+
+    @Mock
     OnPropertyChangedCallback callback;
 
-    @Before
-    public void setup() {
-        subsystem = new TicketSubsystem();
-        callback = mock(OnPropertyChangedCallback.class);
+    @Mock
+    Provider<ReadTicketFromFile> taskProvider;
+
+    @Mock
+    Handler handler;
+
+    @Captor
+    private ArgumentCaptor<Runnable> runnableCaptor;
+
+    @Test
+    public void testConstructor_shouldSetupInitialization() {
+        verify(handler).post(runnableCaptor.capture());
+    }
+
+    @Test
+    public void testConstructor_shouldStartReadTaskWhenInitialized() {
+        ReadTicketFromFile task = mock(ReadTicketFromFile.class);
+        when(taskProvider.get()).thenReturn(task);
+        verify(handler).post(runnableCaptor.capture());
+        Runnable initializationCode = runnableCaptor.getValue();
+        initializationCode.run();
+        verify(task).execute();
     }
 
     @Test
